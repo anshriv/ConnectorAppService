5ba79b8042acfa911aeffe6dee578601f65668eb
kaushiksk <19279785+kaushiksk@users.noreply.github.com>
2022-03-24 12:46:05 +0530

    Add movenext logic

--- RandomConnector/Controllers/Connector.cs
+++ RandomConnector/Controllers/Connector.cs
@@ -13,17 +13,14 @@ namespace RandomConnector.Controllers
     [Route("[controller]")]
     public class Connector : Controller
     {
-        private static readonly string[] Summaries = new[]
-         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private static readonly List<SearchableItem> allItems = GetAllItems();
+        private const int BatchCount = 5;
 
         [HttpGet("id")]
         public Guid GetId()
         {
             return Guid.Parse("d7505861-5050-4f33-b3eb-5de72e9b1cad");
         }
-
 
         [HttpGet("healthCheck")]
         public bool HealthCheck()
@@ -47,38 +44,58 @@ namespace RandomConnector.Controllers
         }
 
         [HttpGet("moveNext")]
-        public List<SearchableItem> MoveNext(string checkPoint)
+        public CheckpointableItem MoveNext(int? checkPoint)
         {
-            if(checkPoint == null)
+            if (checkPoint.HasValue)
             {
-                return new List<SearchableItem>()
+                if (checkPoint.Value < allItems.Count)
                 {
-                    new SearchableItem()
-                    { 
-                        Id = "1",
-                        Properties = new Dictionary<string, object>()
-                        {
-                            {"firstname", "anubhav" },
-                            {"lastname", "shrivastava" }
-                        }
-                    }
-                    ,
-
-                    new SearchableItem()
+                    int count = Math.Min(BatchCount, allItems.Count - checkPoint.Value);
+                    return new CheckpointableItem()
                     {
-                        Id = "2",
-                        Properties = new Dictionary<string, object>()
-                        {
-                            {"firstname", "abhinav" },
-                            {"lastname", "shrivastava" }
-                        }
-                    }
-                };
+                        SearchableItems = allItems.GetRange(checkPoint.Value, count),
+                        Checkpoint = Math.Min(checkPoint.Value + count, allItems.Count),
+                        MoveNext = checkPoint.Value + count < allItems.Count,
+                    };
+                }
+                else
+                {
+                    return new CheckpointableItem()
+                    {
+                        Checkpoint = allItems.Count,
+                        MoveNext = false,
+                    };
+                }
             }
             else
             {
-                return null;
+                return new CheckpointableItem()
+                {
+                    SearchableItems = allItems.GetRange(0, BatchCount),
+                    Checkpoint = BatchCount,
+                    MoveNext = BatchCount < allItems.Count,
+                };
             }
+        }
+
+        private static List<SearchableItem> GetAllItems()
+        {
+            var items = new List<SearchableItem>();
+
+            for (var i = 0; i < 12; i++)
+            {
+                items.Add(new SearchableItem()
+                {
+                    Id = i.ToString(),
+                    Properties = new Dictionary<string, object>()
+                    {
+                        {"firstname", "firstname" + i.ToString()},
+                        {"lastname", "lastname" +  i.ToString()},
+                    }
+                }); ;
+            }
+
+            return items;
         }
     }
 }
